9de5d88115ea15a226a97a15c360983d4ec129fc
zainijaz255 <zainijaz255@gmail.com>
2026-04-15 16:41:22 +0500

    leatest pull from origin

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { persistor, store } from './store/index.js'
+// import { persistor, store } from './store/index.js'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
@@ -10,13 +10,13 @@ import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store} >
-    <PersistGate loading={null} persistor={persistor}>
+  // <Provider store={store} >
+    // <PersistGate loading={null} persistor={persistor}>
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
       </QueryClientProvider>
-    </PersistGate>
-  </Provider >,
+    // </PersistGate>
+  /* </Provider >, */
 )
