94051a7a12065dcd86cdbf08453444566144aef9
ZAIN Ul ABAIDEEN <119429395+zainulabaideen@users.noreply.github.com>
2026-05-01 18:29:29 +0500

    update

--- Server/index.js
+++ Server/index.js
@@ -12,6 +12,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
+  "https://testing.freethemekit.com",
   "https://shego.pk",
   process.env.CLIENT_URL,
 ].filter(Boolean);
